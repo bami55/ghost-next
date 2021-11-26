03d77b1a6bcd8212db6e1109f6fca54e65a36075
bami <1992bami@gmail.com>
2021-11-26 14:18:24 +0900

    記事ページ追加

--- front/pages/api/post.js
+++ front/pages/api/post.js
@@ -1,8 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default async function handler(req, res) {
+  const { slug } = req.query;
   const { BLOG_URL, CONTENT_API_KEY } = process.env;
-  const url = `${BLOG_URL}/ghost/api/v3/content/posts?key=${CONTENT_API_KEY}&fields=title,slug,custom_excerpt,feature_image`;
+  const url = `${BLOG_URL}/ghost/api/v3/content/posts/slug/${slug}?key=${CONTENT_API_KEY}&fields=title,slug,html`;
   const data = await fetch(url).then((res) => res.json());
-  res.status(200).json({ posts: data.posts });
+  res.status(200).json({ post: data.posts[0] });
 }
